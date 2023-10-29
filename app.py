3dce73596e6fc6f0ef0e0ef8681c6ace4efe2070
HashMapsData2Value <83883690+HashMapsData2Value@users.noreply.github.com>
2023-10-29 10:07:19 +0100

    feat: black lint

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import requests
-#import redis
+
+# import redis
 from flask import Flask
 from flask_apscheduler import APScheduler
 from algosdk import encoding
@@ -11,31 +12,37 @@ scheduler.api_enabled = True
 scheduler.init_app(app)
 scheduler.start()
 
-#r = redis.Redis(host='localhost', port=6379, db=0)
+# r = redis.Redis(host='localhost', port=6379, db=0)
 
 accounts = {}
 algonode_api = "https://mainnet-api.algonode.cloud/v2/accounts/{}"
 
-@app.route('/')
-def hello_world():
-    return '<h1>Welcome to account watcher!</h1>'
 
-@app.route('/add/<account>')
+@app.route("/")
+def hello_world():
+    return "<h1>Welcome to account watcher!</h1>"
+
+
+@app.route("/add/<account>")
 def add_account(account):
     if encoding.is_valid_address(account):
         if account not in accounts:
-            accounts[account] = -1 #unless changed will mean -1 means uninitialized account
-#        if r.set(account, -1):
-            update_account(account) #TODO: Error handling here, or async?
-            return "Accepted {}".format(account) #TODO: add proper HTTP code
-#        return "Failed to store {}".format(account) #TODO: add proper HTTP code
-    return "Address not valid: {}".format(account) #TODO: add proper HTTP code
+            accounts[
+                account
+            ] = -1  # unless changed will mean -1 means uninitialized account
+            #        if r.set(account, -1):
+            update_account(account)  # TODO: Error handling here, or async?
+            return "Accepted {}".format(account)  # TODO: add proper HTTP code
+    #        return "Failed to store {}".format(account) #TODO: add proper HTTP code
+    return "Address not valid: {}".format(account)  # TODO: add proper HTTP code
 
-@app.route('/list')
+
+@app.route("/list")
 def list_accounts():
-    return json.dumps(accounts) #TODO: 
+    return json.dumps(accounts)  # TODO:
 
-@scheduler.task('interval', id='update_on_schedule', seconds=60)
+
+@scheduler.task("interval", id="update_on_schedule", seconds=60)
 def update_all_accounts():
     for account in accounts:
         update_account(account)
@@ -43,19 +50,26 @@ def update_all_accounts():
 
 def update_account(account):
     try:
-        amount = query_account_balance(account) #TODO: Error handling here
+        amount = query_account_balance(account)  # TODO: Error handling here
         if accounts[account] != amount:
-            print("Address {} amount changed from {} to {}!".format(account, accounts[account], amount)) #TODO: replace with proper notification/logging
+            print(
+                "Address {} amount changed from {} to {}!".format(
+                    account, accounts[account], amount
+                )
+            )  # TODO: replace with proper notification/logging
         accounts[account] = amount
         return True
     except:
-        print("Failed to update account {}".format(account)) #TODO: replace with proper notification/logging
+        print(
+            "Failed to update account {}".format(account)
+        )  # TODO: replace with proper notification/logging
+
 
 def query_account_balance(account):
     response = requests.get(algonode_api.format(account))
     if response.status_code == 200:
         data = response.json()
         if "amount" in data:
-            return data['amount']
+            return data["amount"]
         raise Exception("No amount in response")
-    return -1 #TODO: Error handling here
+    return -1  # TODO: Error handling here
